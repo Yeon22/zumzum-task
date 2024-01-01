e54b4f9a04bef3fa10d12ddd72218cf6ae83bda7
suyeon <suyeon@subak-MacBook-Pro.local>
2024-01-01 14:42:15 +0900

    CreateCustomerBookingDto 에서 @Trnasform 사용하도록 수정

--- src/customer/dto/customer.dto.ts
+++ src/customer/dto/customer.dto.ts
@@ -1,4 +1,5 @@
-import {IsEnum, IsMobilePhone, IsString} from '@nestjs/class-validator';
+import {IsEnum, IsMobilePhone, IsString } from '@nestjs/class-validator';
+import { Transform } from 'class-transformer';
 import { IsDate, MinDate } from 'class-validator';
 import { BOOKING_STATE } from 'src/booking/entity/booking.entity';
 
@@ -10,11 +11,22 @@ export class CreateCustomerDto {
     phone: string;
 }
 
+const bookingMinDate = () => {
+    const today = new Date();
+    const nextDate = new Date(today.getTime() + 24 * 60 * 60 * 1000);
+    nextDate.setHours(0);
+    nextDate.setMinutes(0);
+    nextDate.setSeconds(0);
+    nextDate.setMilliseconds(0);
+    return nextDate;
+}
+
 export class CreateCustomerBookingDto {
     @IsEnum(BOOKING_STATE)
     state: BOOKING_STATE;
 
+    @Transform(({value}) => new Date(value))
     @IsDate()
-    @MinDate(new Date())
+    @MinDate(bookingMinDate())
     tourStartAt: Date;
 }
