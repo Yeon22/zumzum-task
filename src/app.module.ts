b65b34bbccd6be864989724fb6c1fd5555a5cd17
suyeon <suyeon@subak-MacBook-Pro.local>
2024-01-02 18:42:36 +0900

    redis 환경 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CacheModule } from '@nestjs/cache-manager'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomerModule } from './customer/customer.module';
 import { SellerModule } from './seller/seller.module';
 import { TourModule } from './tour/tour.module';
 import { BookingModule } from './booking/booking.module';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
   imports: [
@@ -18,6 +20,11 @@ import { BookingModule } from './booking/booking.module';
         database: process.env.MYSQL_DB,
         entities: ['dist/**/*.entity{.ts,.js}'],
         synchronize: true,
+    }),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT
     }),
     CustomerModule,
     SellerModule,
